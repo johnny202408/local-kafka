ee2f2ab14a22af9e05b0f00a8593feb815814a95
ggg <ggg@ggg.com>
2024-11-28 12:55:01 +0200

    basic write and read from the local kafka

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-kafka local
+# kafka local
+
+launch the kafka containers:  
+```docker-compose up -d```  
+
+add this entry to /etc/hosts
+127.0.0.1 kafka
+
+### Usage  
+#### Start the stack:  
+Run docker-compose up -d.
+
+#### Access Kafka UI:  
+Visit http://localhost:8080.
+
+#### Access Kafdrop:  
+Visit http://localhost:9000.
+
+prepare the python virtual environment:  
+```python3 -m venv venv
+./venv/bin/activate
+pip install -U -r requirements.txt
+```  
+
+generate json sample data:  
+```python3 generate_samples.py```
+
+stream one sample file into the kafka cluster:  
+```python3 sanity01.py```
+
+
+
---
+++ sanity01.py
@@ -0,0 +1,36 @@
+import json
+from confluent_kafka import Producer
+
+# Configuration for Kafka
+kafka_config = {
+    'bootstrap.servers': 'localhost:9092',  # Adjust if needed
+}
+
+# Create a Kafka producer
+producer = Producer(kafka_config)
+
+def delivery_report(err, msg):
+    """Callback function to handle delivery reports."""
+    if err:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Message delivered to {msg.topic()} [{msg.partition()}]")
+
+def send_json_to_kafka(file_path, topic):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            message = json.dumps(data)
+
+            producer.produce(topic, message, callback=delivery_report)
+            producer.flush()
+
+            print("Message sent successfully!")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    json_file_path = 'sample_data/sample_data_01.json'  # Path to your JSON file
+    kafka_topic = 'topic-try01'
+
+    send_json_to_kafka(json_file_path, kafka_topic)
